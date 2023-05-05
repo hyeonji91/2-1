e56bef6193b461cc43ca219932574488ba05b9cc
hjkim <hjkim1770@gmail.com>
2023-05-06 05:22:31 +0900

    week05

--- week02_bottom_navigation/BottomNavigation Template/app/src/main/java/gachon/third/umc/android/HomeFragment.kt
+++ week02_bottom_navigation/BottomNavigation Template/app/src/main/java/gachon/third/umc/android/HomeFragment.kt
@@ -12,6 +12,12 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import gachon.third.umc.android.databinding.FragmentHomeBinding
 import android.graphics.Rect
+import android.graphics.Typeface
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.StyleSpan
+import gachon.third.umc.android.databinding.ItemPostBinding
 
 class HomeFragment: Fragment() {
 
@@ -52,14 +58,10 @@ class HomeFragment: Fragment() {
         postRecyclerView.adapter = postAdapter
         postRecyclerView.layoutManager = LinearLayoutManager(activity)
 
-        postData.add(PostData("test text ", R.drawable.ic_profile_default, R.drawable.ic_story_border))
-        postData.add(PostData("test text2 ", R.drawable.ic_profile_default, R.drawable.ic_story_border))
-        postData.add(PostData("test text3 ", R.drawable.ic_profile_default, R.drawable.ic_story_border))
-        postRecyclerView.adapter?.notifyDataSetChanged()
-        postRecyclerView.addItemDecoration(RecyclerViewDecoration(30));
-
+        addPost(postData)
 
     }
+
 }
 
 
@@ -85,4 +87,25 @@ fun addStroy(storyData : ArrayList<StoryData>){
     storyData.add(StoryData("user7 ", R.drawable.ic_profile_default, R.drawable.ic_story_border))
     storyData.add(StoryData("user8 ", R.drawable.ic_profile_default, R.drawable.ic_story_border))
     storyData.add(StoryData("user9 ", R.drawable.ic_profile_default, R.drawable.ic_story_border))
+}
+
+fun addPost(postData: ArrayList<PostData>){
+    postData.add(PostData(
+        R.drawable.ic_profile_default,
+        "user1",
+        R.drawable.ic_launcher_background,
+        SpannableString("좋아요 1개\n"+"user1 "+" 안녕하세요 가천대학교 UMC 초코입니다\n"+"댓글 10개 모두보기\n"+"5월5일")
+    ))
+    postData.add(PostData(
+        R.drawable.ic_profile_default,
+        "user2",
+        R.drawable.ic_launcher_background,
+        SpannableString("좋아요 1개\n"+"user2 "+" 안녕하세요 가천대학교 UMC 초코입니다\n"+"댓글 10개 모두보기\n"+"5월5일")
+    ))
+    postData.add(PostData(
+        R.drawable.ic_profile_default,
+        "user3",
+        R.drawable.ic_launcher_background,
+        SpannableString("좋아요 1개\n"+"user3 "+" 안녕하세요 가천대학교 UMC 초코입니다\n"+"댓글 10개 모두보기\n"+"5월5일")
+    ))
 }
--- week02_bottom_navigation/BottomNavigation Template/app/src/main/java/gachon/third/umc/android/PostAdapter.kt
+++ week02_bottom_navigation/BottomNavigation Template/app/src/main/java/gachon/third/umc/android/PostAdapter.kt
@@ -1,6 +1,13 @@
 package gachon.third.umc.android
 
-import android.media.Image
+import android.graphics.Color
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,13 +15,21 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import gachon.third.umc.android.databinding.ItemPostBinding
-import gachon.third.umc.android.databinding.ItemStoryBinding
 
 
+//data class PostData(
+//
+//    val profileData: Int,
+//    val name: String,
+//    val postImage: Int,
+//    val context: String
+//)
 data class PostData(
-    val content: String,
+
     val profileData: Int,
-    val borderData: Int
+    val name: String,
+    val postImage: Int,
+    val context: SpannableString
 )
 
 class PostAdapter(private val dataList: ArrayList<PostData>) : RecyclerView.Adapter<PostAdapter.PostViewHolder>() {
@@ -22,20 +37,36 @@ class PostAdapter(private val dataList: ArrayList<PostData>) : RecyclerView.Adap
 //뷰홀더
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false)
-        return PostViewHolder(ItemStoryBinding.bind(view))
+        return PostViewHolder(ItemPostBinding.bind(view))
     }
 
 //뷰홀더
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) {//실제로 표시하는 역할
-        holder.binding.name.text = dataList[position].content
-        holder.binding.profileImg.setImageResource(dataList[position].profileData)
-        holder.binding.border.setImageResource(dataList[position].borderData)
+
+        holder.binding.profile.setImageResource(dataList[position].profileData)
+        holder.binding.name.text = dataList[position].name
+        holder.binding.postImage.setImageResource(dataList[position].postImage)
+        //holder.binding.context.text = dataList[position].context
+
+
+//spannable이용해서 글자 디자인
+    var context = SpannableString(dataList[position].context)
+
+    val boldStyle = StyleSpan(Typeface.BOLD)
+    val colorBlueSpan = ForegroundColorSpan(Color.GRAY)
+    val sizeBigSpan = RelativeSizeSpan(0.8f)
+
+    context.setSpan(boldStyle, 0 ,12, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    context.setSpan(colorBlueSpan, 35 ,context.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    context.setSpan(sizeBigSpan, context.length-4 ,context.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+    holder.binding.context.text= context
     }
 
     override fun getItemCount(): Int = dataList.size
 
 //뷰홀더
-    inner class PostViewHolder(val binding: ItemStoryBinding): RecyclerView.ViewHolder(binding.root){
+    inner class PostViewHolder(val binding: ItemPostBinding): RecyclerView.ViewHolder(binding.root){
 
     }
 
