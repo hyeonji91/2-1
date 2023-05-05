9b385d899088fba917e83df8bef304a5b54a76fb
hjkim <hjkim1770@gmail.com>
2023-05-05 17:57:46 +0900

    리사이클러뷰 화면+item클릭하면 삭제 구현+ 메모장 다 구현/ 수정만 남음

--- week5/MyMemo2/app/src/main/java/com/example/mymemo2/AddMemoActivity.kt
+++ week5/MyMemo2/app/src/main/java/com/example/mymemo2/AddMemoActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mymemo2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mymemo2.databinding.ActivityAddMemoBinding
 
 class AddMemoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAddMemoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_memo)
+        binding = ActivityAddMemoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.saveBtn.setOnClickListener{
+            val mIntent = Intent(this, MainActivity::class.java)
+            mIntent.putExtra("memo",binding.writeMemo.text.toString())
+            setResult(RESULT_OK, mIntent)
+            if(!isFinishing) finish()
+        }
+
     }
 }
