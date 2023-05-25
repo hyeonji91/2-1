bdd94f3389ff1d0f5b6774d40b1186a11999d00f
hjkim <hjkim1770@gmail.com>
2023-05-26 04:49:52 +0900

    week8 TextWatcher까지 완성

--- week02_bottom_navigation/BottomNavigation Template/app/src/main/java/gachon/third/umc/android/LoginActivity.kt
+++ week02_bottom_navigation/BottomNavigation Template/app/src/main/java/gachon/third/umc/android/LoginActivity.kt
@@ -1,11 +1,84 @@
 package gachon.third.umc.android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import androidx.appcompat.app.AppCompatActivity
+import gachon.third.umc.android.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    private var isHide = true
+    private var isIdEmpty = true
+    private var isPwdEmpty = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //눈 아이콘 눌렀을때 비밀번호 보이게/안보이게
+        binding.btnHidepwd.setOnClickListener{
+            if(isHide){
+                //보이게
+                isHide = false
+                binding.btnHidepwd.setImageResource(R.drawable.ic_pwd_on)
+                binding.pwd.transformationMethod = HideReturnsTransformationMethod.getInstance();
+            } else{
+                isHide = true
+                binding.btnHidepwd.setImageResource(R.drawable.ic_pwd_off)
+                binding.pwd.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+        }
+
+        //id 한 자리 이상인지
+        binding.id.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(binding.id.text.isEmpty()) {
+                    isIdEmpty = true
+                } else {
+                    isIdEmpty = false
+                }
+                setBtnLogin()
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                //if(binding.id.toString().isNotEmpty())
+                //  binding.btnCheck.isEnabled =  true
+            }
+        })
+
+        //pwd 한 자리 이상인지
+        binding.pwd.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if(binding.pwd.text.isEmpty()) {
+                    isPwdEmpty = true
+                } else {
+                    isPwdEmpty = false
+                }
+                setBtnLogin()
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                //if(binding.id.toString().isNotEmpty())
+                //  binding.btnCheck.isEnabled =  true
+            }
+        })
+
+    }
+
+    fun setBtnLogin(){
+        if(isIdEmpty||isPwdEmpty) {
+            binding.btnLogin.isEnabled = false
+            binding.btnLogin.setBackgroundResource(R.drawable.loginbtn_off)
+        }
+        else {
+            binding.btnLogin.isEnabled = true
+            binding.btnLogin.setBackgroundResource(R.drawable.loginbtn_on)
+        }
     }
 }
--- week02_bottom_navigation/BottomNavigation Template/app/src/main/java/gachon/third/umc/android/Sgin/PhoneNumberFragment.kt
+++ week02_bottom_navigation/BottomNavigation Template/app/src/main/java/gachon/third/umc/android/Sgin/PhoneNumberFragment.kt
@@ -7,23 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import gachon.third.umc.android.R
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PhoneNumberFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class PhoneNumberFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(gachon.third.umc.android.ARG_PARAM1)
-            param2 = it.getString(gachon.third.umc.android.ARG_PARAM2)
-        }
-    }
+class PhoneNumberFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,23 +18,4 @@ class PhoneNumberFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_phone_number, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PhoneNumberFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PhoneNumberFragment().apply {
-                arguments = Bundle().apply {
-                    putString(gachon.third.umc.android.ARG_PARAM1, param1)
-                    putString(gachon.third.umc.android.ARG_PARAM2, param2)
-                }
-            }
-    }
 }
